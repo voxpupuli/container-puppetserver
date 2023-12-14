2022a7d315447ed070bc85d5cf83201e7cfdd180
Martin Alfke <ma@betadots.de>
2023-12-14 10:41:09 +0100

    Make setting for admin-api idempotent
    if restarting the server, the shell snippets will run again
    a duplicate admin-api entry causes the puppetserver to not start

--- puppetserver/docker-entrypoint.d/88-enable-cache-delete-api.sh
+++ puppetserver/docker-entrypoint.d/88-enable-cache-delete-api.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
 if [[ "$PUPPETSERVER_ENABLE_ENV_CACHE_DEL_API" == true ]]; then
-  /opt/puppetlabs/puppet/bin/ruby /add_cache_del_api_auth_rules.rb
+  if [ $(grep 'puppet-admin-api' /etc/puppetlabs/puppetserver/conf.d/auth.conf) ]; then
+    echo "Admin API already set"
+  else
+    /opt/puppetlabs/puppet/bin/ruby /add_cache_del_api_auth_rules.rb
+  fi
 fi

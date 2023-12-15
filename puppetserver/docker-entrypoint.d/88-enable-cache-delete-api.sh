1031e3ceffd184c6dea1ab6e400b804e4662b524
Martin Alfke <ma@betadots.de>
2023-12-15 09:43:13 +0100

    make setting of env api endpoint idempotent
    the script was not correct

--- puppetserver/docker-entrypoint.d/88-enable-cache-delete-api.sh
+++ puppetserver/docker-entrypoint.d/88-enable-cache-delete-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 if [[ "$PUPPETSERVER_ENABLE_ENV_CACHE_DEL_API" == true ]]; then
-  if [ $(grep 'puppet-admin-api' /etc/puppetlabs/puppetserver/conf.d/auth.conf) ]; then
+  if [[ $(grep 'puppet-admin-api' /etc/puppetlabs/puppetserver/conf.d/auth.conf) ]]; then
     echo "Admin API already set"
   else
     /opt/puppetlabs/puppet/bin/ruby /add_cache_del_api_auth_rules.rb

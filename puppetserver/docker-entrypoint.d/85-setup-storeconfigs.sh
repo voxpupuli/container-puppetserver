46a4616a0418139d2cc87f107fb53c567766f3ed
Robert Waffen <rwaffen@gmail.com>
2024-07-19 11:27:23 +0200

    fix: remove general autosigning and switch to server in puppet config commands
    Signed-off-by: Robert Waffen <rwaffen@gmail.com>

--- puppetserver/docker-entrypoint.d/85-setup-storeconfigs.sh
+++ puppetserver/docker-entrypoint.d/85-setup-storeconfigs.sh
@@ -3,24 +3,24 @@
 set -e
 
 if [ -n "$PUPPET_STORECONFIGS_BACKEND" ]; then
-  puppet config set storeconfigs_backend $PUPPET_STORECONFIGS_BACKEND --section master
+  puppet config set storeconfigs_backend $PUPPET_STORECONFIGS_BACKEND --section server
 fi
 
 if [ -n "$PUPPET_STORECONFIGS" ]; then
-  puppet config set storeconfigs $PUPPET_STORECONFIGS --section master
+  puppet config set storeconfigs $PUPPET_STORECONFIGS --section server
 fi
 
 if [ -n "$PUPPET_REPORTS" ]; then
-  puppet config set reports $PUPPET_REPORTS --section master
+  puppet config set reports $PUPPET_REPORTS --section server
 fi
 
 # reset defaults if USE_PUPPETDB is false, but don't overwrite custom settings
 if [ "$USE_PUPPETDB" = 'false' ]; then
   if [ "$PUPPET_REPORTS" = 'puppetdb' ]; then
-    puppet config set reports log --section master
+    puppet config set reports log --section server
   fi
 
   if [ "$PUPPET_STORECONFIGS_BACKEND" = 'puppetdb' ]; then
-    puppet config set storeconfigs false --section master
+    puppet config set storeconfigs false --section server
   fi
 fi

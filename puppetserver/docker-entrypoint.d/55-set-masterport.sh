d3f6e46471f90314ba760f972bb1aa17a67510bb
Stefan - Zipkid - Goethals <stefan@zipkid.eu>
2024-10-24 15:31:34 +0200

    Also set serverport in puppet.conf
    That way, local commands, like `puppetserver ca <cmd>` also work.
    https://www.puppet.com/docs/puppet/8/configuration#serverport

--- puppetserver/docker-entrypoint.d/55-set-masterport.sh
+++ puppetserver/docker-entrypoint.d/55-set-masterport.sh
@@ -9,5 +9,7 @@ hocon() {
 if test -n "$PUPPETSERVER_PORT"; then
   cd /etc/puppetlabs/puppetserver/conf.d/
   hocon -f webserver.conf set webserver.ssl-port $PUPPETSERVER_PORT
+  cd /etc/puppetlabs/puppet/
+  hocon -f puppet.conf set serverport $PUPPETSERVER_PORT
   cd /
 fi

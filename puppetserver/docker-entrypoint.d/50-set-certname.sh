126bf903bf6f969b71796e7e059bc0d246b9a53a
Robert Waffen <rwaffen@gmail.com>
2024-11-08 14:19:21 +0100

    fix: use relative command path

--- puppetserver/docker-entrypoint.d/50-set-certname.sh
+++ puppetserver/docker-entrypoint.d/50-set-certname.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -n "${PUPPETSERVER_HOSTNAME}" ]; then
-  /opt/puppetlabs/bin/puppet config set server "$PUPPETSERVER_HOSTNAME"
+  puppet config set server "$PUPPETSERVER_HOSTNAME"
 fi
 
 if [ -n "${CERTNAME}" ]; then
-  /opt/puppetlabs/bin/puppet config set certname "$CERTNAME"
+  puppet config set certname "$CERTNAME"
 fi

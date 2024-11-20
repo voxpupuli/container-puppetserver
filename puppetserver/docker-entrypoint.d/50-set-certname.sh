73b617581487ddde4102bbc79663ce31720076cf
Robert Waffen <rwaffen@gmail.com>
2024-11-20 14:48:07 +0100

    fix: set path to puppet bin

--- puppetserver/docker-entrypoint.d/50-set-certname.sh
+++ puppetserver/docker-entrypoint.d/50-set-certname.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -n "${PUPPETSERVER_HOSTNAME}" ]; then
-  puppet config set server "$PUPPETSERVER_HOSTNAME"
+  /usr/bin/puppet config set server "$PUPPETSERVER_HOSTNAME"
 fi
 
 if [ -n "${CERTNAME}" ]; then
-  puppet config set certname "$CERTNAME"
+  /usr/bin/puppet config set certname "$CERTNAME"
 fi

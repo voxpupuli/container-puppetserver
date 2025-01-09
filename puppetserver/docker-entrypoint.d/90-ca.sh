8dca39b3f9b33780d856d54cf6fac683b0e3afbe
Martin Alfke <ma@betadots.de>
2025-01-09 13:58:51 +0100

    Manage ca_ttl setting

--- puppetserver/docker-entrypoint.d/90-ca.sh
+++ puppetserver/docker-entrypoint.d/90-ca.sh
@@ -32,6 +32,9 @@ EOF
   hocon -f webserver.conf set webserver.ssl-crl-path $ssl_crl_path
   cd /
 
+  if [[ -z $CA_TTL ]]; then
+    puppet config set --section server ca_ttl "${CA_TTL}"
+
   # bootstrap certs for the puppetserver
   if [[ ! -f "$ssl_cert" ]]; then
     while ! ca_running; do
